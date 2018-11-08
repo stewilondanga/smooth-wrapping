77464c01ff82844d92fd39cf1939ea10ebe6fe63
Stewart ilondanga <stilondanga@gmail.com>
2018-11-08 17:57:43 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -19,33 +19,33 @@ let nodeCnt = 0, // Node count: Loop through nodes[]
 for (nodeCnt = 0; nodeCnt < totalNodes; nodeCnt++) {
   node = nodes[nodeCnt];
 
-  /*  dupe = node.cloneNode(true);        // We'll clone each node so it can "follow" its sibling `node` element around the UI.
-    dupe.classList.remove("item-node"); // .item-node | visibility: hidden - Will move natively with Flex wrapping (snappy!).
-    dupe.classList.add("item-dupe");    // .item-dupe | visibility: visible - Will smoothly follow sibling .item-node around.
+  dupe = node.cloneNode(true); // We'll clone each node so it can "follow" its sibling `node` element around the UI.
+  dupe.classList.remove("item-node"); // .item-node | visibility: hidden - Will move natively with Flex wrapping (snappy!).
+  dupe.classList.add("item-dupe"); // .item-dupe | visibility: visible - Will smoothly follow sibling .item-node around.
 
-    node.parentNode.appendChild(dupe);  // Position: `absolute` - Each clone stays relative to their shared parent container.
+  /*    node.parentNode.appendChild(dupe);  // Position: `absolute` - Each clone stays relative to their shared parent container.
 
-    dupe.style.top = node.offsetTop + 'px';   // Establish each dupe's `absolute` position.
-    dupe.style.left = node.offsetLeft + 'px'; // <--^
+      dupe.style.top = node.offsetTop + 'px';   // Establish each dupe's `absolute` position.
+      dupe.style.left = node.offsetLeft + 'px'; // <--^
 
-    boxes[nodeCnt] = { node, dupe };
-  }
+      boxes[nodeCnt] = { node, dupe };
+    }
 
-  function moveNodes() {
-    clearTimeout(resizeWaitID);
-    resizeWaitID = setTimeout(() => {
-      for (nodeCnt = 0; nodeCnt < totalNodes; nodeCnt++) {
-        boxes[nodeCnt].dupe.style.left = boxes[nodeCnt].node.offsetLeft + 'px';
-        boxes[nodeCnt].dupe.style.top = boxes[nodeCnt].node.offsetTop + 'px';
-      }
-    }, 101);
-  }
+    function moveNodes() {
+      clearTimeout(resizeWaitID);
+      resizeWaitID = setTimeout(() => {
+        for (nodeCnt = 0; nodeCnt < totalNodes; nodeCnt++) {
+          boxes[nodeCnt].dupe.style.left = boxes[nodeCnt].node.offsetLeft + 'px';
+          boxes[nodeCnt].dupe.style.top = boxes[nodeCnt].node.offsetTop + 'px';
+        }
+      }, 101);
+    }
 
-  window.addEventListener("resize", moveNodes);
+    window.addEventListener("resize", moveNodes);
 
-  const divSwitch = document.querySelector('.container');
+    const divSwitch = document.querySelector('.container');
 
-  document.querySelector('.button').addEventListener('click', e => {
-    moveNodes();
-    divSwitch.classList.toggle('switch');
-  });
+    document.querySelector('.button').addEventListener('click', e => {
+      moveNodes();
+      divSwitch.classList.toggle('switch');
+    });
